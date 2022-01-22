4f6e06af38ce7e167578e7975c81f5dba4d27b5e
Osmolovskiy Yuriy <j-thrash@ya.ru>
2022-01-22 23:41:26 +0300

    123

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+        TicTacToe board = new TicTacToe();
+        System.out.println(board.makeMove(1, 1));
+        System.out.println(board.makeMove(1, 1));
+        System.out.println(board.makeMove(1, 2));
+        System.out.println(board.makeMove(2, 1));
+        System.out.println(board.makeMove(2, 2));
+        System.out.println(board.makeMove(3, 1));
+        System.out.println(board.makeMove(2, 2));
     }
 }
